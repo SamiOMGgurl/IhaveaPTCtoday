0490a78769e7a3beefd817adf25da4854272fba7
SamiOMGgurl <83353299+SamiOMGgurl@users.noreply.github.com>
2023-07-03 10:46:30 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+song = "";
+song2 = "";
+letWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+
+function preload()
+{
+song = loadSound("music.mp3");
+song2 = loadSound("music 2.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(540, 300);
+    canvas.position(370,200);
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+function modelLoaded()
+{
+    console.log('Ready!');
+    poseNet.on('pose', gotPoses);
+}
+
+function draw()
+{
+    image(video, 0, 0, 540, 300);
+}
+
+function music()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
+}
